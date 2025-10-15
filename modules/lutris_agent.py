be1d07c963db1015645c405c633067d2c9cb89fe
Rambo <ramborigs90@gmail.com>
2025-10-15 12:36:21 -0400

    added standalone functionality

--- modules/lutris_agent.py
+++ modules/lutris_agent.py
@@ -1,17 +1,26 @@
-from modules.config import IGDB_CLIENT, IGDB_TOKEN
-from modules.igdb import IGDBClient
+# Attempt relative import for use as a module within a package structure
+try:
+    from .config import IGDB_CLIENT, IGDB_TOKEN
+    from .igdb import IGDBClient
+# Fall back to direct import which assumes the script is being ran standalone
+except ImportError:
+    from config import IGDB_CLIENT, IGDB_TOKEN
+    from igdb import IGDBClient
 import json
 
-igdb = IGDBClient(IGDB_CLIENT, IGDB_TOKEN)
+def fetch_game_info(game):
+    igdb = IGDBClient(IGDB_CLIENT, IGDB_TOKEN)
+    game_info = igdb.search_game(game)
+    if game_info:
+        # Sanitized response
+        print(f"Found {game_info['name']}")
+        print(game_info['summary'])
+        print(game_info['release_date'])
+        print(game_info['genres'])
+        print(game_info['developers'])
+        # Raw response
+        #print(json.dumps(game_info, indent=2))
 
-game_info = igdb.search_game("My Summer Car")
-
-if game_info:
-    # Sanitized response
-    print(f"Found {game_info['name']}")
-    print(game_info['summary'])
-    print(game_info['release_date'])
-    print(game_info['genres'])
-    print(game_info['developers'])
-    # Raw response
-    #print(json.dumps(game_info, indent=2))
+if __name__ == "__main__":
+    game = "My Summer Car"
+    fetch_game_info(game)
