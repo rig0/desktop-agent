141a69cd02d737ae84a6bdf21990bcbb82be921a
Rambo <ramborigs90@gmail.com>
2025-10-24 17:40:52 -0400

    fixed indentation on config file

--- modules/config.py
+++ modules/config.py
@@ -29,45 +29,45 @@ except FileNotFoundError:
 # ----------------------------
 def create_default_config(config_path):
     config_content = """# ================== DESKTOP AGENT CONFIG ==================
-    # These are the default values.
-    # Intervals are in seconds.
-    # Modules are disabled by default.
-    # Documentation: https://github.com/rig0/hass-desktop-agent
+# These are the default values.
+# Intervals are in seconds.
+# Modules are disabled by default.
+# Documentation: https://github.com/rig0/hass-desktop-agent
 
-    # If you enable the game agent, create an igdb.com account and fill your api credentials.
-    # Read more https://api-docs.igdb.com/#authentication (Access token, not client secret!)
+# If you enable the game agent, create an igdb.com account and fill your api credentials.
+# Read more https://api-docs.igdb.com/#authentication (Access token, not client secret!)
 
-    [device]
-    name = Device-Name
-    interval = 10
+[device]
+name = Device-Name
+interval = 10
 
-    # Required!
-    [mqtt]
-    broker = homeassistant-ip
-    port = 1883
-    username = username
-    password = password
+# Required!
+[mqtt]
+broker = homeassistant-ip
+port = 1883
+username = username
+password = password
 
-    # Optional
-    [modules]
-    api = false
-    commands = false
-    media_agent = false
-    game_agent = false
-    updates = false
+# Optional
+[modules]
+api = false
+commands = false
+media_agent = false
+game_agent = false
+updates = false
 
-    # Module Options
-    [api]
-    port = 5555
+# Module Options
+[api]
+port = 5555
 
-    [updates]
-    interval = 3600
+[updates]
+interval = 3600
 
-    # Required for game_agent
-    [igdb]
-    client_id = your_igdb_client_id
-    token = your_igdb_access_token
-    """
+# Required for game_agent
+[igdb]
+client_id = your_igdb_client_id
+token = your_igdb_access_token
+"""
 
     # Write to file
     with open(config_path, "w") as f:
