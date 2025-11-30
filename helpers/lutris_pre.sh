0c8dd920ce74d91251b6cdf54440323735f6dbac
Rigo <rigo-dev@pm.me>
2025-11-30 13:27:33 -0500

    fix for new game_monitor dir

--- helpers/lutris_pre.sh
+++ helpers/lutris_pre.sh
@@ -4,13 +4,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Define GAME_NAME_FILE path assuming it lives in the parent directory of the script.
-GAME_NAME_FILE="$SCRIPT_DIR/../data/game_agent/current_game"
+GAME_NAME_FILE="$SCRIPT_DIR/../data/game_monitor/current_game"
 
 # Write the GAME_NAME to the designated file.
 echo "$GAME_NAME" > "$GAME_NAME_FILE"
 
 # Fetch the env from lutris
-#env > "$SCRIPT_DIR/../data/game_agent/lutris_prelaunch_env.txt"
+#env > "$SCRIPT_DIR/../data/game_monitor/lutris_prelaunch_env.txt"
 
 # Log the launch event with additional information (optional).
-#echo "Launching $GAME_NAME ($LUTRIS_GAME_UUID) [$STORE]" >> "$SCRIPT_DIR/../data/game_agent/lutris_launch.log"
+#echo "Launching $GAME_NAME ($LUTRIS_GAME_UUID) [$STORE]" >> "$SCRIPT_DIR/../data/game_monitor/lutris_launch.log"
